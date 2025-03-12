f2529f277ae9b4955adceb36b867a68ef130a1c7
SaeedKarimian860 <SGK12190@gmail.com>
2025-03-12 19:54:11 -0400

    Travel List App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+export default function App() {}
 
-function App() {
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+
+function Form() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="add-form">
+      <h3>What do you need for you 😍 trip?</h3>
     </div>
   );
 }
 
-export default App;
+function PackingList() {}
+
+function Stats() {}
