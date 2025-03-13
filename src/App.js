3204ba5790378e3c684f7d7df27232ada18cf1c2
SaeedKarimian860 <SGK12190@gmail.com>
2025-03-13 19:43:31 -0400

    Travel List App

--- src/App.js
+++ src/App.js
@@ -19,7 +19,9 @@ function PackingList() {
 function Stats() {
   return (
     <footer>
+      <em>
       🧳 You have X items on your list, and you already packed X(X%)
+      </em>
     </footer>
   );
 }
