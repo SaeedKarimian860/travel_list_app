a3b66e64cd049fb13617b82501a121e6cfa05ada
SaeedKarimian860 <SGK12190@gmail.com>
2025-03-13 19:51:20 -0400

    Travel List App

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function PackingList() {
 
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>
       🧳 You have X items on your list, and you already packed X(X%)
       </em>
