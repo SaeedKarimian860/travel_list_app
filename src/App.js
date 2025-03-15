2881e0a871cce44efc0c3ad740f0eab7acd63c0c
SaeedKarimian860 <SGK12190@gmail.com>
2025-03-15 14:13:40 -0400

    Travel List App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 
 export default function App() {
@@ -18,10 +19,9 @@ function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>;
 }
 
-
 function Form() {
   function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
   }
 
   return (
